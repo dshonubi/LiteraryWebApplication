32384653089d049bf8b6ef2294562db900eec80d
dshonubi <shonubi64@googlemail.com>
2020-09-11 09:51:49 +0100

    Restructuring project and starting to design the Homepage using Bootstrap

--- src/main/java/com/litageview/controller/LiteraryWebApplicationMain.java
+++ src/main/java/com/litageview/controller/LiteraryWebApplicationMain.java
@@ -1,10 +1,14 @@
 package com.litageview.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LiteraryWebApplicationMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	@RequestMapping("/")
+	public String theHomepage() {
+		return "homepage";
 	}
 
 }
